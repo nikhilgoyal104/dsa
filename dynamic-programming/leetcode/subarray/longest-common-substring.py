599677d7efdfb60c9c5b573466775b721b071c06
nikhilgoyal104 <nikhilgoyal104ah4@gmail.com>
2021-08-24 12:05:49 +0530

    update

--- dynamic-programming/leetcode/subarray/longest-common-substring.py
+++ dynamic-programming/leetcode/subarray/longest-common-substring.py
@@ -1,14 +1,33 @@
+# T=2ⁿ,S=n
 def x(s1, s2):
     m, n = len(s1), len(s2)
 
-    def dfs(i, j):
+    def dfs(i, j, count):
         if i == m or j == n:
-            return 0
+            return count
         if s1[i] == s2[j]:
-            return 1 + dfs(i + 1, j + 1)
-        return max(dfs(i + 1, j), dfs(i, j + 1))
+            count = dfs(i + 1, j + 1, count + 1)
+        return max(count, dfs(i + 1, j, 0), dfs(i, j + 1, 0))
 
-    return dfs(0, 0)
+    return dfs(0, 0, 0)
+
+
+# T=mn,S=mn
+def y(s1, s2):
+    m, n, dp = len(s1), len(s2), {}
+
+    def dfs(i, j, count):
+        if i == m or j == n:
+            return count
+        key = i, j, count
+        if key in dp:
+            return dp[key]
+        if s1[i] == s2[j]:
+            count = dfs(i + 1, j + 1, count + 1)
+        dp[key] = max(count, dfs(i + 1, j, 0), dfs(i, j + 1, 0))
+        return dp[key]
+
+    return dfs(0, 0, 0)
 
 
 for s1, s2 in [
@@ -18,4 +37,5 @@ for s1, s2 in [
     ('abcdxyz', 'xyzabcd'),
     ('zxabcdezy', 'yzabcdezx')
 ]:
-    print(x(s1, s2))
+    print(x(s1, s2), end=' ')
+    print(y(s1, s2))
