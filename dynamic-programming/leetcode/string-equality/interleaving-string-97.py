99a9c2a34ecf9b11bd6dd6a69b77987cdcedd4e5
nikhilgoyal104 <nikhilgoyal104ah4@gmail.com>
2021-08-25 07:06:54 +0530

    update

--- dynamic-programming/leetcode/string-equality/interleaving-string-97.py
+++ dynamic-programming/leetcode/string-equality/interleaving-string-97.py
@@ -10,13 +10,12 @@ def x(s1, s2, s3):
         key = i, j
         if key in dp:
             return dp[key]
-        dp[key] = False
+        f1 = f2 = False
         if i < m and s1[i] == s3[k]:
-            dp[key] = dfs(i + 1, j, k + 1)
-        elif j < n and s2[j] == s3[k]:
-            dp[key] = dfs(i, j + 1, k + 1)
-        elif i < m and j < n and s1[i] == s2[j] == s3[k]:
-            dp[key] = dfs(i + 1, j, k + 1) or dfs(i, j + 1, k + 1)
+            f1 = dfs(i + 1, j, k + 1)
+        if j < n and s2[j] == s3[k]:
+            f2 = dfs(i, j + 1, k + 1)
+        dp[key] = f1 or f2
         return dp[key]
 
     return dfs(0, 0, 0)
