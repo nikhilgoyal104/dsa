7c8ddae54880a05810ab933ec77b5b1c4aac36c4
nikhilgoyal104 <nikhilgoyal104ah4@gmail.com>
2021-10-10 10:27:01 +0530

    update

--- arrays/leetcode/product-of-array-except-self-238.py
+++ arrays/leetcode/product-of-array-except-self-238.py
@@ -1,5 +1,25 @@
-# T=n,S=n
+# T=n,S=1
 def x(nums):
+    n = len(nums)
+    zeros = zeroIndex = 0
+    product = 1
+    for i, val in enumerate(nums):
+        if val == 0:
+            zeros += 1
+            zeroIndex = i
+            if zeros > 1:
+                return [0] * n
+        else:
+            product *= val
+    res = [0] * n
+    if zeros == 1:
+        res[zeroIndex] = product
+        return res
+    return [product // val for val in nums]
+
+
+# T=n,S=n
+def y(nums):
     n = len(nums)
     res, left, right = [1] * n, [1] * n, [1] * n
     for i in range(1, n):
@@ -12,7 +32,7 @@ def x(nums):
 
 
 # T=n,S=1
-def y(nums):
+def z(nums):
     n = len(nums)
     res = [1] * n
     for i in range(1, n):
@@ -25,9 +45,12 @@ def y(nums):
 
 
 for nums in [
+    [1, 2, 3, 0, 5, 6, 0],
     [-1, 1, 0, -3, 3],
     [4, 5, 1, 8, 2],
     [1, 2, 3, 4]
 ]:
     print(x(nums))
     print(y(nums))
+    print(z(nums))
+    print()
