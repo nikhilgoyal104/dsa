fb6bff2d922061c1f576584966c316c76aabd3f1
nikhilgoyal104 <nikhilgoyal104ah4@gmail.com>
2021-10-30 23:15:14 +0530

    update

---
+++ trees/binary/leetcode/path-sum/path-sum-II-113.py
@@ -0,0 +1,24 @@
+from binarytree import build2
+
+
+def main(root):
+    def dfs(root, sum):
+        if not root:
+            return []
+        if root.left is root.right:
+            return [[root.val]] if sum + root.val == target else []
+        res = []
+        for child in [root.left, root.right]:
+            for path in dfs(child, sum + root.val):
+                res.append([root.val] + path)
+        return res
+
+    return dfs(root, 0)
+
+
+for root, target in [
+    (build2([5, 4, 8, 11, None, 13, 4, 7, 2, None, None, 5, 1]), 22),
+    (build2([1, 2, 3]), 5),
+    (build2([1, 2]), 0)
+]:
+    print(main(root))
