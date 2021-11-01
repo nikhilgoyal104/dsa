05997379a238c5ea07e82de2b5747e2210465cfd
nikhilgoyal104 <nikhilgoyal104ah4@gmail.com>
2021-11-01 23:41:11 +0530

    update

--- trees/binary/leetcode/all-nodes-distance-k-in-binary-tree-863.py
+++ trees/binary/leetcode/all-nodes-distance-k-in-binary-tree-863.py
@@ -1,38 +1,50 @@
 from binarytree import build2
 
-root = build2([3, 5, 1, 6, 2, 0, 8, None, None, 7, 4])
-print(root)
+r1 = build2([3, 5, 1, 6, 2, 0, 8, None, None, 7, 4])
+r2 = build2([0, 1, None, 3, 2])
+r3 = build2([1])
 
 
-def ancestors(root, target):
+def getRootToNodePath(root, target):
     def dfs(root):
         if not root:
             return []
         if root == target:
-            return [root.val]
+            return [root]
         for child in [root.left, root.right]:
             path = dfs(child)
             if path:
-                return [root.val] + path
+                return [root] + path
         return []
 
     return dfs(root)
 
 
-def distance(root, k):
-    if not root:
-        return []
-    if not k:
-        return [root.val]
-    return distance(root.left, k - 1) + distance(root.right, k - 1)
+def getNodesKDistAway(root, blockedNode, k):
+    def dfs(root, k):
+        if not root or root is blockedNode:
+            return []
+        if not k:
+            return [root.val]
+        return dfs(root.left, k - 1) + dfs(root.right, k - 1)
+
+    return dfs(root, k)
 
 
 def main(root, target, k):
-    print(distance(target, k))
-    print(ancestors(root, target))
+    res, blockedNode = [], None
+    rootToNodePath = getRootToNodePath(root, target)
+    while rootToNodePath and k:
+        node = rootToNodePath.pop()
+        res += getNodesKDistAway(node, blockedNode, k)
+        blockedNode = node
+        k -= 1
+    return res
 
 
 for root, target, k in [
-    (root, root.left, 2)
+    (r1, r1.left, 2),
+    (r2, r2.left.right, 1),
+    (r3, r3, 3),
 ]:
-    main(root, target, k)
+    print(main(root, target, k))
