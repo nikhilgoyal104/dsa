2cb16eac44ae7e6e3b1780fbeceee9c707dbcfef
nikhilgoyal104 <nikhilgoyal104ah4@gmail.com>
2021-11-03 00:41:28 +0530

    update

--- trees/bst/leetcode/serialize-and-deserialize-bst-449.py
+++ trees/bst/leetcode/serialize-and-deserialize-bst-449.py
@@ -7,23 +7,15 @@ class Codec:
         def dfs(root):
             if not root:
                 return []
-            return dfs(root.left) + [root.val] + dfs(root.right)
+            return [root.val] + dfs(root.left) + dfs(root.right)
 
         return ','.join(map(str, dfs(root)))
 
     def deserialize(self, data):
-        inorder = data.split(',')
+        def dfs():
+            pass
 
-        def dfs(low, high):
-            if low > high:
-                return None
-            mid = low + (high - low) // 2
-            root = TreeNode(int(inorder[mid]))
-            root.left = dfs(low, mid - 1)
-            root.right = dfs(mid + 1, high)
-            return root
-
-        return dfs(0, len(inorder) - 1)
+        return dfs()
 
 
 codec = Codec()
@@ -31,7 +23,9 @@ for root in [
     build2([50, 25, 75, 12, 37, 62, 87, None, None, 30, None, None, 70]),
     build2([4, 2, 6, 1, 3, 5, 7]),
     build2([2, 1, 3]),
-    build2([])
+    build2([3, 1, 4, None, 2, None, 5]),
+    build2([2, 1]),
+    build2([1])
 ]:
     print(root)
     data = codec.serialize(root)
