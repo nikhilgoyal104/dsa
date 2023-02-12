4ea1367d36c120570c1d4336b01fcb0b9e3a5a7e
Nikhil Goyal <nikhilgl@amazon.com>
2023-02-12 16:21:44 +0530

    refactor

--- arrays/leetcode/trapping-rain-water-42.py
+++ arrays/leetcode/trapping-rain-water-42.py
@@ -1,13 +1,15 @@
 # T=n,S=n
 def main(nums):
     n = len(nums)
-    leftMax, rightMax = [0] * n, [0] * n
-    leftMax[0], rightMax[-1] = nums[0], nums[-1]
+    res = 0
+    leftMax = [0] * n
+    leftMax[0] = nums[0]
     for i in range(1, n):
         leftMax[i] = max(leftMax[i - 1], nums[i])
+    rightMax = [0] * n
+    rightMax[-1] = nums[-1]
     for i in range(n - 2, -1, -1):
         rightMax[i] = max(rightMax[i + 1], nums[i])
-    res = 0
     for i in range(1, n - 1):
         res += min(leftMax[i], rightMax[i]) - nums[i]
     return res
@@ -15,7 +17,6 @@ def main(nums):
 
 for nums in [
     [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1],
-    [4, 2, 0, 3, 2, 5],
     [3, 0, 0, 2, 0, 4]
 ]:
     print(main(nums))
--- misc/grid/spiral-matrix-54.py
+++ misc/grid/spiral-matrix-54.py
@@ -1,28 +1,28 @@
 # T=mn,S=1
 def main(grid):
+    res = []
     m, n = len(grid), len(grid[0])
     minRi = minCi = 0
     maxRi, maxCi = m - 1, n - 1
-    res, total = [], m * n
     while True:
         for ci in range(minCi, maxCi + 1):
             res.append(grid[minRi][ci])
-        if len(res) == total:
+        if len(res) == m * n:
             break
         minRi += 1
         for ri in range(minRi, maxRi + 1):
             res.append(grid[ri][maxCi])
-        if len(res) == total:
+        if len(res) == m * n:
             break
         maxCi -= 1
         for ci in range(maxCi, minCi - 1, -1):
             res.append(grid[maxRi][ci])
-        if len(res) == total:
+        if len(res) == m * n:
             break
         maxRi -= 1
         for ri in range(maxRi, minRi - 1, -1):
             res.append(grid[ri][minCi])
-        if len(res) == total:
+        if len(res) == m * n:
             break
         minCi += 1
     return res
