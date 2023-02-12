9a97c69d3339686cdb7eba5829c1363dd2a1bc39
Nikhil Goyal <nikhilgl@amazon.com>
2023-02-12 14:47:20 +0530

    refactor

---
+++ misc/math/reverse-integer-7.py
@@ -0,0 +1,19 @@
+# T=logn,S=1
+def main(n):
+    res = 0
+    factor = -1 if n < 0 else 1
+    range = 2 ** 31
+    n = abs(n)
+    while n:
+        digit = n % 10
+        res = 10 * res + digit
+        n //= 10
+    if res < -range or res > range:
+        return 0
+    return factor * res
+
+
+for n in [
+    123, -123, 120, 0, 1534236469
+]:
+    print(main(n))
