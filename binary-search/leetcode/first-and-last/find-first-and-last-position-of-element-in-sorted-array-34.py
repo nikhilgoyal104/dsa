7ace8fc731fe98f9471a4e6a37a2edf6ede7f820
Nikhil Goyal <nikhilgl@amazon.com>
2023-03-05 14:51:38 +0530

    refactor

--- binary-search/leetcode/first-and-last/find-first-and-last-position-of-element-in-sorted-array-34.py
+++ binary-search/leetcode/first-and-last/find-first-and-last-position-of-element-in-sorted-array-34.py
@@ -1,27 +1,31 @@
 # T=logn,S=1
 def main(nums, target):
     def getFirstPosition():
-        low, high, res = 0, len(nums) - 1, -1
+        res = -1
+        low, high = 0, len(nums) - 1
         while low <= high:
             mid = low + (high - low) // 2
-            if target > nums[mid]:
-                low = mid + 1
-            elif target <= nums[mid]:
+            if target == nums[mid]:
+                res = mid
                 high = mid - 1
+            elif target > nums[mid]:
+                low = mid + 1
             else:
-                res, high = mid, mid - 1
+                high = mid - 1
         return res
 
     def getLastPosition():
-        low, high, res = 0, len(nums) - 1, -1
+        res = -1
+        low, high = 0, len(nums) - 1
         while low <= high:
             mid = low + (high - low) // 2
-            if target > nums[mid]:
+            if target == nums[mid]:
+                res = mid
                 low = mid + 1
-            elif target < nums[mid]:
-                high = mid - 1
+            elif target > nums[mid]:
+                low = mid + 1
             else:
-                res, low = mid, mid + 1
+                high = mid - 1
         return res
 
     firstPosition = getFirstPosition()
