9f516b52e61ebfde363ded6200f3bf829d3a0f2c
Nikhil Goyal <nikhilgl@amazon.com>
2023-04-14 12:11:28 +0530

    refactor

--- heap/leetcode/the-kth-factor-of-n-1492.py
+++ heap/leetcode/the-kth-factor-of-n-1492.py
@@ -14,7 +14,7 @@ inputs = [
 def main(n, k):
     res = 0
     for i in range(1, n + 1):
-        if n % i == 0:
+        if not n % i:
             res += 1
             if res == k:
                 return i
