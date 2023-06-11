f4ed88aab789a9eaf4be5e2fabca82128e0b7884
Nikhil Goyal <nikhilgl@amazon.com>
2023-06-11 22:13:30 +0530

    refactor

--- recursion/subsets/subsets-iterative.py
+++ recursion/subsets/subsets-iterative.py
@@ -1,6 +1,7 @@
 def x(nums):
-    res, n = [], len(nums)
-    for i in range(pow(2, n)):
+    n = len(nums)
+    res = []
+    for i in range(2 ** n):
         val = []
         for j in range(n):
             if i & (1 << j):
@@ -10,11 +11,14 @@ def x(nums):
 
 
 def y(nums):
-    res, n = [], len(nums)
+    n = len(nums)
+    res = []
     for i in range(2 ** n):
-        num, val = i, []
+        num = i
+        val = []
         for j in range(n):
-            quot, rem = divmod(num, 2)
+            rem = num % 2
+            num //= 2
             if rem:
                 val.append(nums[j])
         res.append(val)
@@ -24,5 +28,5 @@ def y(nums):
 for nums in [
     [1, 2, 3]
 ]:
-    print(x(nums))
-    print(y(nums))
+    print(str(nums) + '->' + str(x(nums)))
+    print(str(nums) + '->' + str(y(nums)))
